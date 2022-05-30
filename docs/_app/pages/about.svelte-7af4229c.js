7209adbc91c6508ee1665b38f6373b6a6b237de1
Connor McGwire <cdmcgwire@live.com>
2022-05-30 10:41:02 -0700

    setting initial site content

---
+++ docs/_app/pages/about.svelte-7af4229c.js
@@ -0,0 +1,39 @@
+import{S as J,i as K,s as Q,e as o,t as w,k as _,c as r,a as s,h as b,d as a,m as j,b as i,g as U,F as t,n as M}from"../chunks/index-1aa3af76.js";function X(G){let n,h,c,d,E,k,u,e,m,q,A,f,T,P,p,z,H,g,W,x,y,B;return{c(){n=o("article"),h=o("header"),c=o("div"),d=o("h1"),E=w("About Me"),k=_(),u=o("section"),e=o("div"),m=o("p"),q=w(`I am a mom of three and grandma of four. My husband and I recently\r
+				relocated to the Tucson area to escape the wet climate of the Pacific\r
+				Northwest. Before becoming a real estate agent, I spent my career life\r
+				in aviation.`),A=_(),f=o("p"),T=w(`I grew up, hanging out at the airport with my dad, who was a maintenance\r
+				director for a corporate flight department. I always enjoyed helping my\r
+				dad work on airplanes and I continued to help him even after I had\r
+				children. While raising my three children was my focus, when my husband\r
+				was laid off from his job after 9/11, I turned my hobby business of\r
+				cleaning airplanes into a full-time business. I continued working in\r
+				corporate and general aviation until we had the opportunity to move to\r
+				Arizona.`),P=_(),p=o("p"),z=w(`Being self-employed, I was easily able to work homeschooling our\r
+				children around my work. Homeschooling gave us a great opportunity to\r
+				enjoy our children to the fullest, even on the hard days. Homeschooling\r
+				our kids is one of my best life accomplishments.`),H=_(),g=o("p"),W=w(`The time we have with our kids is so short and it wasn't long before\r
+				they were all out of our home and established in their own homes. When\r
+				our daughter and family decided to move to a state with a lower cost of\r
+				living, we realized it was time for us to simplify our life. We had\r
+				friends in Arizona and enjoyed visiting here in the past. This has been\r
+				a great relocation adventure.`),x=_(),y=o("p"),B=w(`I am so glad I became a real estate agent here and am ready to help you\r
+				in your real estate transaction.`),this.h()},l(I){n=r(I,"ARTICLE",{class:!0});var v=s(n);h=r(v,"HEADER",{class:!0});var C=s(h);c=r(C,"DIV",{class:!0});var D=s(c);d=r(D,"H1",{class:!0});var N=s(d);E=b(N,"About Me"),N.forEach(a),D.forEach(a),C.forEach(a),k=j(v),u=r(v,"SECTION",{class:!0});var S=s(u);e=r(S,"DIV",{class:!0});var l=s(e);m=r(l,"P",{class:!0});var R=s(m);q=b(R,`I am a mom of three and grandma of four. My husband and I recently\r
+				relocated to the Tucson area to escape the wet climate of the Pacific\r
+				Northwest. Before becoming a real estate agent, I spent my career life\r
+				in aviation.`),R.forEach(a),A=j(l),f=r(l,"P",{class:!0});var V=s(f);T=b(V,`I grew up, hanging out at the airport with my dad, who was a maintenance\r
+				director for a corporate flight department. I always enjoyed helping my\r
+				dad work on airplanes and I continued to help him even after I had\r
+				children. While raising my three children was my focus, when my husband\r
+				was laid off from his job after 9/11, I turned my hobby business of\r
+				cleaning airplanes into a full-time business. I continued working in\r
+				corporate and general aviation until we had the opportunity to move to\r
+				Arizona.`),V.forEach(a),P=j(l),p=r(l,"P",{class:!0});var F=s(p);z=b(F,`Being self-employed, I was easily able to work homeschooling our\r
+				children around my work. Homeschooling gave us a great opportunity to\r
+				enjoy our children to the fullest, even on the hard days. Homeschooling\r
+				our kids is one of my best life accomplishments.`),F.forEach(a),H=j(l),g=r(l,"P",{class:!0});var L=s(g);W=b(L,`The time we have with our kids is so short and it wasn't long before\r
+				they were all out of our home and established in their own homes. When\r
+				our daughter and family decided to move to a state with a lower cost of\r
+				living, we realized it was time for us to simplify our life. We had\r
+				friends in Arizona and enjoyed visiting here in the past. This has been\r
+				a great relocation adventure.`),L.forEach(a),x=j(l),y=r(l,"P",{class:!0});var O=s(y);B=b(O,`I am so glad I became a real estate agent here and am ready to help you\r
+				in your real estate transaction.`),O.forEach(a),l.forEach(a),S.forEach(a),v.forEach(a),this.h()},h(){i(d,"class","svelte-1gaqjce"),i(c,"class","center-col svelte-1gaqjce"),i(h,"class","svelte-1gaqjce"),i(m,"class","svelte-1gaqjce"),i(f,"class","svelte-1gaqjce"),i(p,"class","svelte-1gaqjce"),i(g,"class","svelte-1gaqjce"),i(y,"class","svelte-1gaqjce"),i(e,"class","center-col svelte-1gaqjce"),i(u,"class","svelte-1gaqjce"),i(n,"class","svelte-1gaqjce")},m(I,v){U(I,n,v),t(n,h),t(h,c),t(c,d),t(d,E),t(n,k),t(n,u),t(u,e),t(e,m),t(m,q),t(e,A),t(e,f),t(f,T),t(e,P),t(e,p),t(p,z),t(e,H),t(e,g),t(g,W),t(e,x),t(e,y),t(y,B)},p:M,i:M,o:M,d(I){I&&a(n)}}}class Z extends J{constructor(n){super(),K(this,n,null,X,Q,{})}}export{Z as default};
