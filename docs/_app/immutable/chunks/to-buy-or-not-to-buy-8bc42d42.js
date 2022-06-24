6c3ad4737c515f3cfff1d3b35ad5e4969de7b8fb
Connor McGwire <cdmcgwire@live.com>
2022-06-24 13:55:27 -0700

    publishing blog

---
+++ docs/_app/immutable/chunks/to-buy-or-not-to-buy-8bc42d42.js
@@ -0,0 +1,51 @@
+import{S as F,i as O,s as Y,D as W,x as z,y as C,z as H,A as R,B as j,r as D,p as G,C as X,X as q,e as f,k as v,t as k,c as m,a as g,d as a,m as x,h as I,Y as J,b as M,g as r,F as w,n as K}from"./index-3685d269.js";import{B as L}from"./blog-264767c4.js";function Q(c){let t,o,l,u,n,i,d,y,T,_,h,$,b,p,S;return{c(){t=f("p"),o=f("img"),u=v(),n=f("p"),i=k(`No matter the market, we are always perplexed by the decision to buy or to not
+buy real estate. If it\u2019s the decision between renting and owning, we weigh the
+costs of the purchase against the costs of maintenance. If it\u2019s the decision to
+invest, the balancing act of tax advantages against return on investment plagues
+our thinking process. No matter our situation, we should be seeking advice from
+our trusted advisors. Who do you trust to help you consider your options? Where
+are you getting your information?`),d=v(),y=f("p"),T=k(`I always start my decision making process by understanding my terms. By
+acknowledging these fundamental steps in your decision making process, you can
+avoid being swayed or distracted by erroneous data and information. You can make
+valid analysis of your options. We can understand the basic differences between
+renting and owning, but how is rental income taxed differently than owner
+occupied real estate?`),_=v(),h=f("p"),$=k(`How ownership is taken and how the property will be used must be considered
+before any transaction decision can be made. Investment property does not meet
+all loan qualifications. Rental properties are taxed differently. Married
+couples may buy property to own separately yet state laws may negate the terms
+of the contract if the right terms are not clearly defined. Some people will not
+buy a house with a contract. This is an interesting situation that does come up
+from time to time. I personally will never sell a home without a contract. This
+protects the buyer and the seller in full disclosure of their obligation and it
+helps define the terms. Who, or what, is your trusted advisor to help you in
+your specific real estate decision?`),b=v(),p=f("p"),S=k(`Find a resource, or several resources, you can rely on with the help of a real
+estate agent. A real estate agent cannot, and should not, advise beyond their
+expertise. They can, and should, be the source for your source. In considering
+what real estate agent suits your needs, consider how they are advising you. An
+indication of a good real estate agent is how they help you find the resources
+you need to make your decisions.`),this.h()},l(e){t=m(e,"P",{});var s=g(t);o=m(s,"IMG",{src:!0,alt:!0}),s.forEach(a),u=x(e),n=m(e,"P",{});var P=g(n);i=I(P,`No matter the market, we are always perplexed by the decision to buy or to not
+buy real estate. If it\u2019s the decision between renting and owning, we weigh the
+costs of the purchase against the costs of maintenance. If it\u2019s the decision to
+invest, the balancing act of tax advantages against return on investment plagues
+our thinking process. No matter our situation, we should be seeking advice from
+our trusted advisors. Who do you trust to help you consider your options? Where
+are you getting your information?`),P.forEach(a),d=x(e),y=m(e,"P",{});var A=g(y);T=I(A,`I always start my decision making process by understanding my terms. By
+acknowledging these fundamental steps in your decision making process, you can
+avoid being swayed or distracted by erroneous data and information. You can make
+valid analysis of your options. We can understand the basic differences between
+renting and owning, but how is rental income taxed differently than owner
+occupied real estate?`),A.forEach(a),_=x(e),h=m(e,"P",{});var E=g(h);$=I(E,`How ownership is taken and how the property will be used must be considered
+before any transaction decision can be made. Investment property does not meet
+all loan qualifications. Rental properties are taxed differently. Married
+couples may buy property to own separately yet state laws may negate the terms
+of the contract if the right terms are not clearly defined. Some people will not
+buy a house with a contract. This is an interesting situation that does come up
+from time to time. I personally will never sell a home without a contract. This
+protects the buyer and the seller in full disclosure of their obligation and it
+helps define the terms. Who, or what, is your trusted advisor to help you in
+your specific real estate decision?`),E.forEach(a),b=x(e),p=m(e,"P",{});var N=g(p);S=I(N,`Find a resource, or several resources, you can rely on with the help of a real
+estate agent. A real estate agent cannot, and should not, advise beyond their
+expertise. They can, and should, be the source for your source. In considering
+what real estate agent suits your needs, consider how they are advising you. An
+indication of a good real estate agent is how they help you find the resources
+you need to make your decisions.`),N.forEach(a),this.h()},h(){J(o.src,l="/img/blog/entryway.jpg")||M(o,"src",l),M(o,"alt","Our life is frittered away by detail. Simplify. Simplify.")},m(e,s){r(e,t,s),w(t,o),r(e,u,s),r(e,n,s),w(n,i),r(e,d,s),r(e,y,s),w(y,T),r(e,_,s),r(e,h,s),w(h,$),r(e,b,s),r(e,p,s),w(p,S)},p:K,d(e){e&&a(t),e&&a(u),e&&a(n),e&&a(d),e&&a(y),e&&a(_),e&&a(h),e&&a(b),e&&a(p)}}}function U(c){let t,o;const l=[c[0],B];let u={$$slots:{default:[Q]},$$scope:{ctx:c}};for(let n=0;n<l.length;n+=1)u=W(u,l[n]);return t=new L({props:u}),{c(){z(t.$$.fragment)},l(n){C(t.$$.fragment,n)},m(n,i){H(t,n,i),o=!0},p(n,[i]){const d=i&1?R(l,[i&1&&j(n[0]),i&0&&j(B)]):{};i&2&&(d.$$scope={dirty:i,ctx:n}),t.$set(d)},i(n){o||(D(t.$$.fragment,n),o=!0)},o(n){G(t.$$.fragment,n),o=!1},d(n){X(t,n)}}}const B={title:"To Buy or Not to Buy",date:"2022-06-24",layout:"blog"};function V(c,t,o){return c.$$set=l=>{o(0,t=W(W({},t),q(l)))},t=q(t),[t]}class Z extends F{constructor(t){super(),O(this,t,V,U,Y,{})}}var ne=Object.freeze(Object.defineProperty({__proto__:null,default:Z,metadata:B},Symbol.toStringTag,{value:"Module"}));export{Z as T,ne as _,B as m};
---
+++ docs/_app/immutable/pages/blog/posts/to-buy-or-not-to-buy.md-96220984.js
@@ -0,0 +1,1 @@
+import"../../../chunks/index-3685d269.js";import"../../../chunks/blog-264767c4.js";import{T as r,m as e}from"../../../chunks/to-buy-or-not-to-buy-8bc42d42.js";export{r as default,e as metadata};
